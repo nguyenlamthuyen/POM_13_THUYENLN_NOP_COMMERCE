1f60ba4230eaab5886e3a10b9b229c3f500530e9
Thuyen Nguyen <nguyenlamthuyen309@gmail.com>
2020-03-17 01:34:25 +0700

    Topic 30: Multi browser and Parallel

--- actions/commons/AbstractTest.java
+++ actions/commons/AbstractTest.java
@@ -1,5 +1,41 @@
 package commons;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 public class AbstractTest {
+	private WebDriver driver;
+	
+	public WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", ".\\browserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", ".\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("headless_chrome")) {
+			System.setProperty("webdriver.chrome.driver", ".\\browserDrivers\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(options);
+		} else if (browserName.equalsIgnoreCase("headless_firefox")) {
+			System.setProperty("webdriver.gecko.driver", ".\\browserDrivers\\geckodriver.exe");
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new FirefoxDriver(options);
+		}
+		
+		driver.get(GlobalConstants.DEV_URL);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		return driver;
+	}
 
 }
